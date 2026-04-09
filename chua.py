a0dedd48671242d6d68c1c005defbfd008bf984a
Ilweran <quizblow@gmail.com>
2026-04-09 22:39:07 +0200

    Coloured and aligned the Chua system equations.

--- chua.py
+++ chua.py
@@ -23,20 +23,26 @@ class ChuaSimulation(ThreeDScene):
         initial_state = [0.1, 0.1, 0.1]
         t = np.arange(0, 100, 0.01)
         states = odeint(chua_system, initial_state, t)
-        points = [np.array([x, y, z]) for x, y, z in states]
+        
+        # Scale to fit
+        max_val = np.max(np.abs(states))
+        scale_factor = 3.5 / max_val
+        scaled_data = states * scale_factor
+
+        points = [np.array([s[0], s[1], s[2]]) for s in scaled_data]
 
         # 3. Setup Scene
         curve = VMobject().set_points_smoothly(points)
-        curve.set_stroke(color=YELLOW, width=0.5, opacity=0.8)
+        curve.set_stroke(color=YELLOW, width=1.5, opacity=0.8)
 
         def create_3d_glow(dot_center, color=YELLOW, layers=15):
             glow = Group()
             for i in range(1, layers + 1):
                 # Larger radius and lower opacity for outer layers
-                opacity = 0.15 * (1 - i / layers)
+                opacity = 0.12 * (1 - i / layers)
                 layer = Dot3D(
                     point=dot_center,
-                    radius=0.05 + (i * 0.04), 
+                    radius=0.03 + (i * 0.02), 
                     color=color
                 ).set_opacity(opacity)
                 glow.add(layer)
@@ -48,39 +54,35 @@ class ChuaSimulation(ThreeDScene):
         # Updater to make glow follow the growing end of the curve
         glow_dot.add_updater(lambda m: m.move_to(curve.get_end()))
 
-        axes = ThreeDAxes()
-        self.set_camera_orientation(phi=75 * DEGREES, theta=10 * DEGREES)
-        
         # Convert points to Manim vectors
-        points = [np.array([x, y, z]) for x, y, z in states]
+        # points = [np.array([x, y, z]) for x, y, z in states]
         path = VMobject().set_points_as_corners(points)
         path.set_stroke(color=YELLOW, width=0.5, opacity=0.8)
 
         # Add Chua equations
-        equations = Tex(R"""
-            \[
-            \begin{aligned}
-            \frac{dx}{dt} &= \alpha \left( y - x - f(x) \right) \\
-            \frac{dy}{dt} &= x - y + z \\
-            \frac{dz}{dt} &= -\beta y
-            \end{aligned}
-            f(x) = m_1 x + \frac{1}{2}(m_0 - m_1)\left(|x + 1| - |x - 1|\right)
-            \]
-            """,
-            tex_to_color_map={
-                "x": RED,
-                "y": GREEN,
-                "z": BLUE,
-            },
-            font_size=25
-        )
+        tex_args = {"substrings_to_isolate": ["x", "y", "z"]}
+
+        eq1 = MathTex(r"\frac{dx}{dt} = \alpha ( y - x - f(x) )", **tex_args)
+        eq2 = MathTex(r"\frac{dy}{dt} = x - y + z", **tex_args)
+        eq3 = MathTex(r"\frac{dz}{dt} = -\beta y", **tex_args)
+        eq4 = MathTex(r"f(x) = m_1 x + \frac{1}{2}(m_0 - m_1)(|x + 1| - |x - 1|)", **tex_args)
+        
+        equations = VGroup(eq1, eq2, eq3, eq4).arrange(DOWN, aligned_edge=LEFT, buff=0.2)
+        equations.scale(0.6).to_corner(UL, buff=0.5)
+
+        # Apply colors
+        for eq in equations:
+            eq.set_color_by_tex("x", RED)
+            eq.set_color_by_tex("y", GREEN)
+            eq.set_color_by_tex("z", BLUE)
+
+        self.set_camera_orientation(phi=75 * DEGREES, theta=-45 * DEGREES)
         self.add_fixed_in_frame_mobjects(equations)
-        equations.to_corner(UL)
-        self.play(Write(equations))
 
 
         # 4. Animation
-        self.add(ThreeDAxes(), glow_dot)
-        self.begin_ambient_camera_rotation(rate=0.1) # Add some cinematic spin
-        self.play(Create(curve), run_time=30, rate_func=linear)
-        self.wait(2)
+        self.play(Write(equations))
+        self.add(glow_dot)
+        self.begin_ambient_camera_rotation(rate=0.2, about="theta")
+        self.play(Create(curve), run_time=40, rate_func=linear)
+        self.wait(3)
